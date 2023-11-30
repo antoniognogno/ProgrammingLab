bc286e603f6093f620f743070094647580d4c7b7
antoniognogno <78216046+antoniognogno@users.noreply.github.com>
2023-11-30 12:13:35 +0000

    Lezione4

---
+++ Lezione4.py
@@ -0,0 +1,20 @@
+class CSVFile():
+    def __init__(self, file_name):
+        self.name = file_name
+        self.file = open(self.name, 'r')
+
+    def get_data(self):
+        result = []
+        values = []
+        for line in self.file:
+            line = line.split(',')
+            if line[0] != 'Date':
+                values.append(line)
+                result.append(values)
+                values.clear
+        return values
+
+
+        
+fileCSV = CSVFile("shampoo_sales.csv")
+print(fileCSV.get_data())
